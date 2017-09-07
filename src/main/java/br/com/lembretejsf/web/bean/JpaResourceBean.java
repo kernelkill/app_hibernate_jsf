b4fc68bef15711f99b9e6263a233749833a43f30
Kachorroski <kernelkill@outlook.com>
2017-09-07 16:58:12 -0400

    JpaResourceBean foi implementada.

--- src/main/java/br/com/lembretejsf/web/bean/JpaResourceBean.java
+++ src/main/java/br/com/lembretejsf/web/bean/JpaResourceBean.java
@@ -1,6 +1,18 @@
 package br.com.lembretejsf.web.bean;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaResourceBean {
 
+    private static EntityManagerFactory entityManagerFactory;
+
+    private static EntityManagerFactory getEntityManagerFactory(){
+
+        if (entityManagerFactory == null){
+            entityManagerFactory = Persistence.createEntityManagerFactory("lembretejsf_db");
+        }
+        return entityManagerFactory;
+    }
 
 }
